35570c80e4b1da1b9895f65133f634d76329ee8e
Daria Kolpakova <daka.kolp01@gmail.com>
2024-11-27 10:26:50 +0200

    SCENARIO 3

--- HomeWork3part2.playground/Contents.swift
+++ HomeWork3part2.playground/Contents.swift
@@ -252,7 +252,7 @@ class Screen {
             
             resultStringToPrint += "\n\(index + 1) "
             resultStringToPrint += "Назва продукту: \(product.name), "
-            resultStringToPrint += "Ціна: \(currency.rawValue) \(String(format: "%.2f", product.price))"
+            resultStringToPrint += "Ціна: \(currency.rawValue) \(String(format: "%.2f", productprice))"
         }
         
         resultStringToPrint += "\n---------------------------------------------------------------------"
@@ -409,7 +409,7 @@ screen.printCheck(cart: cart)
 
 // Початок коду сценарію для Пункт 3
 
-/*
+
 
 print("\nSCENARIO 3:\n")
 
@@ -423,7 +423,7 @@ screen.printCart(cart: cart, currency: .usd)
 // Користувач натискає кнопку зміни валюти на EUR
 screen.printCart(cart: cart, currency: .eur)
 
- */
+
  
 // Кінець коду сценарію для Пункт 3
 
