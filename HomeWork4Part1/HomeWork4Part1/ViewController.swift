4fa9f407402b70e59a71bf41fdf76f49916622c9
Daria Kolpakova <daka.kolp01@gmail.com>
2024-12-01 21:11:12 +0200

    Зроблено завдання 4.1

--- HomeWork4Part1/HomeWork4Part1/ViewController.swift
+++ HomeWork4Part1/HomeWork4Part1/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        label.text = "Hello"
     }
 
-
+    @IBAction func onButtonClicked(_ sender: Any) {
+        label.text = "Bye"
+    }
 }
 
