0b39588d882621d0134d7ca4f20c31c3143321d0
Daria Kolpakova <daka.kolp01@gmail.com>
2024-12-07 19:22:20 +0200

    HomeWork6UK: implemented

--- UIKit/HomeWork6UK/HomeWork6UK/ViewController.swift
+++ UIKit/HomeWork6UK/HomeWork6UK/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var discountText: UILabel!
+    @IBOutlet weak var productName: UILabel!
+    @IBOutlet weak var productCode: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    @IBOutlet weak var buyOnCreditButton: UIButton!
+    @IBOutlet weak var buyNowButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setBuyOnCreditStyle()
+        setProductInfo()
     }
-
-
+    
+    @IBAction func buyNowButtonClicked(_ sender: Any) {
+        print("Buy Now Clicked")
+    }
+    
+    @IBAction func buyOnCreditButtonClicked(_ sender: Any) {
+        print("Buy On Credit Clicked")
+    }
+    
+    private func setBuyOnCreditStyle() {
+        buyOnCreditButton.layer.borderColor = UIColor.lightGray.cgColor
+        buyOnCreditButton.layer.masksToBounds = true
+        buyOnCreditButton.layer.borderWidth = 1.0
+        buyOnCreditButton.layer.cornerRadius = 8.0
+    }
+    
+    private func setProductInfo() {
+        productImage.image = UIImage(named: "img")
+        discountText.text = "-10%"
+        productName.text = "Motherboard, Asus ROG Strix B550-E Gaming (s AM4, AMD B550, PCI-Ex16))"
+        productCode.text = "Code 218525893"
+        productPrice.text = "1000"
+    }
 }
 
