ee827567aad00b6194664529c591b94200ae2f2b
Daria Kolpakova <daka.kolp01@gmail.com>
2024-12-21 19:17:43 +0200

    HW9: initial

---
+++ Lesson9HW/Lesson9HW/Source/Screen/SignUpScreen/SignUpViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SignUpViewController.swift
+//  Lesson9HW
+//
+
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    @IBOutlet weak var contentView: SignUpView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        subscrubeNotifications()
+    }
+}
+
