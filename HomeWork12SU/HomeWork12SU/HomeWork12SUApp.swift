079cd711f16b896ac5a229496e90b8efd5fc460a
Daria Kolpakova <daka.kolp01@gmail.com>
2025-01-02 19:33:10 +0200

    HW12: initial

---
+++ HomeWork12SU/HomeWork12SU/HomeWork12SUApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork12SUApp.swift
+//  HomeWork12SU
+//
+//  Created by Daria Kolpakova on 02.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork12SUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HomeWork12UK/HomeWork12UK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork12UK
+//
+//  Created by Daria Kolpakova on 02.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
